baf8c5c8d48a159eb310d85cd8e58211e58b944d
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2017-12-11 15:21:03 -0500

    added script for checking name and email

--- cyan/assets/js/form_check.js
+++ cyan/assets/js/form_check.js
@@ -1,19 +1,51 @@
-
-
-function Check(){
-	// let el = document.getElementById('name');
+function nameCheck(){
+	let name = document.getElementById('name');
   if (name.value === ""){
-    alert("empty");
+    $(".name").css({"background-color" : "#ff496a1a"});
+    return false;
+  } else {
+    $(".name").css({"background-color" : "rgba(144, 144, 144, 0.075)"});
+    return true;
   }
+}
 
+function emailCheck(mail){
+  let email = document.getElementById("email");
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)){
+    $(".email").css({"background-color" : "rgba(144, 144, 144, 0.075)"});
+    return true;
+  } else {
+    $(".email").css({"background-color" : "#ff496a1a"});
+    return false;
+  }
 }
 
 function check(){
-  let el = document.activeElement;
+  let name = document.getElementById("name");
+  if (name){
+    name.addEventListener("focusout", nameCheck);
+  }
 
-  if (el){
-    el.addEventListener("focusout", Check);
+  let email = document.getElementById("email");
+  if (email){
+    email.addEventListener("focusout", emailCheck);
+  }
+
+  let message = document.getElementById("message");
+  if (message){
+    message.addEventListener("focusout", messageCheck);
   }
 }
+
+function emptyCheck(el){
+  if (el.value === ""){
+    $(".email").css({
+        "color" : "#609D29"
+      });
+  }
+}
+
+
+
 
 check();
